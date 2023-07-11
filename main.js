0a162989eb818ccce45e58b13c84abb84b7d0d2c
nishan akalanka <hdnakalanka@gmail.com>
2023-07-11 12:40:13 +0530

    completed contact

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right="-200px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwYiFSa5PpX0JLAqUw3D7EVAj4qnlxSe0Dz0lrMOIa5Gz9fT9buIEBrIBRVbHd3TrP4hw/exec'
+const form = document.forms['submit-to-google-sheet'];
+const msg=document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML="Message sent successfully";
+            setTimeout(function (){
+                msg.innerHTML=""
+            },1000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
